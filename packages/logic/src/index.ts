8c42abf6f8701a111c2ed49b66f7ed7b4ea6340a
Jesteban1983 <joonathanesteban@protonmail.com>
2026-06-29 20:30:52 +0000

    Avance

---
+++ packages/logic/src/index.ts
@@ -0,0 +1,8 @@
+// packages/logic/src/index.ts
+
+// Exportamos todo lo que está dentro de trackflow
+export * from "./trackflow/collections";
+export * from "./trackflow/contracts";
+export * from "./trackflow/search";
+export * from "./trackflow/transformations";
+export * from "./trackflow/validations";
